f95161450e2930636bc14d4de6af4476f7595862
Rhodri Cusack <cusackrh@kelvin01.cluster>
2021-10-10 06:26:42 +0100

    testing fmriprep confounds pipeline

---
+++ foundcog_preproc.py
@@ -0,0 +1,185 @@
+
+import os
+import json
+import nipype.interfaces.fsl as fsl 
+from nipype.interfaces.spm import Smooth
+
+from nipype.interfaces.utility import IdentityInterface
+from nipype.interfaces.io import SelectFiles, DataSink
+from nipype.algorithms.rapidart import ArtifactDetect
+import nipype.algorithms.confounds as confounds
+from nipype import Workflow, Node, MapNode
+from bids.layout import BIDSLayout
+from niworkflows.interfaces.confounds import NormalizeMotionParams
+from fmriprep.workflows.bold.confounds import init_bold_confs_wf
+from fmriprep import config
+from os import path
+
+
+experiment_dir = '/projects/pi-cusackrh/HPC_18_01039/foundcog/bids'
+output_dir = 'deriv'
+working_dir = 'workingdir'
+
+layout = BIDSLayout(experiment_dir)
+
+# list of subject identifiers
+subject_list = layout.get_subjects()
+task_list = layout.get_tasks()
+session_list = layout.get_sessions()
+
+# # TR of functional images
+with open(path.join(experiment_dir,'task-pictures_bold.json'), 'rt') as fp:
+    task_info = json.load(fp)
+TR = task_info['RepetitionTime']
+
+print(f'Subjects {subject_list}')
+print(f'Sessions {session_list}')
+print(f'Tasks {task_list}')
+print(f'TR is {TR}')
+
+# # Isometric resample of functional images to voxel size (in mm)
+# iso_size = 4
+# 
+
+# Make a list of functional steps to do
+iter_items= {'sub' : [], 'ses':[], 'task': []}
+
+# Template for func files
+func_file_run_1 = path.join('sub-{subject_id}', 'ses-{session}', 'func', 'sub-{subject_id}_ses-{session}_dir-AP_task-{task_name}_run-001_bold.nii.gz')
+func_file_allruns = path.join('sub-{subject_id}', 'ses-{session}', 'func', 'sub-{subject_id}_ses-{session}_dir-AP_task-{task_name}_run-*_bold.nii.gz')
+
+for sub in subject_list:
+    for ses in session_list:
+        for task in task_list:
+            info = {'subject_id':sub, 'session':ses, 'task_name':task}
+            target_file = func_file_run_1.format(**info)
+            if path.isfile(path.join(experiment_dir, target_file)):
+                iter_items['sub'].append(sub)
+                iter_items['ses'].append(ses)
+                iter_items['task'].append(task)
+
+
+# Setup
+omp_nthreads = 8
+mem_gb = {"filesize": 1, "resampled": 1, "largemem": 1}
+
+# MAIN WORKFLOW
+
+# INPUT DATA
+infosource = Node(IdentityInterface(fields=['subject_id', 'session', 'task_name']),
+                  name="infosource")
+infosource.iterables = [('subject_id', iter_items['sub']),
+                        ('session', iter_items['ses']),
+                        ('task_name', iter_items['task'])]
+infosource.synchronize = True # synchronised stepping through each of the iterable lists
+
+# SelectFiles - to grab the data (alternativ to DataGrabber)
+
+templates = {'func': func_file_allruns}
+selectfiles = Node(SelectFiles(templates,
+                               base_directory=experiment_dir),
+                   name="selectfiles")
+
+# Motion correction
+#  extract reference for motion correction
+#   TODO: extract robust reference in noisy infant data?
+extract_ref = Node(interface=fsl.ExtractROI(t_size=1), name='extractref')
+
+def getmiddlevolume(func):
+    from nibabel import load
+    funcfile = func
+    if isinstance(func, list):
+        funcfile = func[0]
+    _, _, _, timepoints = load(funcfile).shape
+    return int(timepoints / 2) - 1
+
+#  MCFLIRT
+motion_correct = Node(fsl.MCFLIRT(mean_vol=True, save_plots=True,output_type='NIFTI'), name="mcflirt")
+
+#  Plot output
+plot_motion = MapNode(
+    interface=fsl.PlotMotionParams(in_source='fsl'),
+    name='plot_motion',
+    iterfield=['in_file'])
+plot_motion.iterables = ('plot_type', ['rotations', 'translations'])
+
+# # Summaries
+# calc_fwd = Node(
+#     interface=ni_confounds.FramewiseDisplacement(parameter_source='FSL'),
+#     name='calc_fwd'
+# )
+# calc_dvars = Node(
+#     interface=ni_confounds.ComputeDVARS(save_all=True, remove_zerovariance=True),
+#     name='calc_dvars'
+# )
+
+# Normalize motion to SPM format
+normalize_motion = Node(NormalizeMotionParams(format='FSL'),
+                               name="normalize_motion")
+
+# 
+bold_confounds_wf = init_bold_confs_wf(mem_gb=mem_gb['largemem'],
+                                        metadata={},
+                                        regressors_all_comps=False,
+                                        regressors_dvars_th=1.5,
+                                        regressors_fd_th=0.5
+                                        )
+
+# Datasink - creates output folder for important outputs
+datasink = Node(DataSink(base_directory=experiment_dir,
+                         container=output_dir),
+                name="datasink")
+
+# Create a preprocessing workflow
+preproc = Workflow(name='preproc')
+preproc.base_dir = path.join(experiment_dir, working_dir)
+
+# Connect all components of the preprocessing workflow
+# preproc.connect([
+#         (infosource, selectfiles, [('subject_id', 'subject_id'),
+#                                    ('session', 'session'),
+#                                    ('task_name', 'task_name')]),
+#         (selectfiles, 'func', motion_correct,  'in_file'),
+#         (selectfiles, ('func', getmiddlevolume), extract_ref, 't_min'),
+#         (extract_ref, 'roi_file', motion_correct, 'ref_file'),
+#         (motion_correct, 'movpar_file', plot_motion, 'in_file'),
+#         (motion_correct,  datasink, [('out_file', 'timeseries')]),
+#         (motion_correct,  datasink, [('movpar_file', 'motion_parameters')]),
+# ])
+
+preproc.connect([(infosource, selectfiles, [('subject_id', 'subject_id'),
+                                   ('session', 'session'),
+                                   ('task_name', 'task_name')])
+                                   ])
+preproc.connect(selectfiles, 'func', motion_correct,  'in_file')
+preproc.connect(selectfiles, 'func', extract_ref, 'in_file')
+preproc.connect(selectfiles, ('func', getmiddlevolume), extract_ref, 't_min')
+preproc.connect(extract_ref, 'roi_file', motion_correct, 'ref_file')
+preproc.connect([(motion_correct, normalize_motion, [('par_file', 'in_file')])])
+preproc.connect(motion_correct, 'par_file', plot_motion, 'in_file')
+# preproc.connect(motion_correct, 'par_file', calc_fwd, 'in_file')
+# preproc.connect(motion_correct, 'par_file', calc_dvars, 'in_file')
+preproc.connect([(plot_motion,  datasink, [('out_file', 'motion_plots')])])
+preproc.connect([(motion_correct,  datasink, [('out_file', 'timeseries')])])
+preproc.connect([(motion_correct,  datasink, [('par_file', 'motion_parameters')])])
+preproc.connect([(motion_correct,  bold_confounds_wf, [('par_file','inputnode.movpar_file')])])
+preproc.connect([(motion_correct,  bold_confounds_wf, [('out_file', 'inputnode.bold')])])
+# preproc.connect([(calc_fwd, datasink, [('out_file', 'motion_statistics')] )])
+# preproc.connect([(calc_dvars, datasink, [('out_file', 'motion_statistics')] )])
+preproc.connect([(bold_confounds_wf, datasink, [('outputnode.confounds_file', 'confounds_file'),
+                                                ('outputnode.confounds_metadata','confounds_metadata')])])
+
+# Create preproc output graph
+preproc.write_graph(graph2use='colored', format='png', simple_form=True)
+
+# Visualize the graph
+from IPython.display import Image
+Image(filename=path.join(preproc.base_dir, 'preproc', 'graph.png'))
+# Visualize the detailed graph
+preproc.write_graph(graph2use='flat', format='png', simple_form=True)
+Image(filename=path.join(preproc.base_dir, 'preproc', 'graph_detailed.png'))
+
+preproc.run()
+
+# preproc.run(plugin='SLURMGraph', plugin_args = {'dont_resubmit_completed_jobs': True})
+#'MultiProc', plugin_args={'n_procs': 8})
---
+++ heuristic.py
@@ -0,0 +1,50 @@
+import os
+from os import path
+
+def create_key(template, outtype=('nii.gz',), annotation_classes=None):
+    if template is None or not template:
+        raise ValueError('Template must be a valid format string')
+    return template, outtype, annotation_classes
+def infotodict(seqinfo):
+    """Heuristic evaluator for determining which runs belong where
+    allowed template fields - follow python string module:
+    item: index within category
+    subject: participant id
+    seqitem: run number during scanning
+    subindex: sub index within group
+    """
+    anat = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_run-00{item:01d}_T1w')
+    func_video = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_dir-AP_task-videos_run-00{item:01d}_bold')
+    func_pictures = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_dir-AP_task-pictures_run-00{item:01d}_bold')
+    func_rest5 = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_dir-AP_task-rest5_run-00{item:01d}_bold')
+    func_rest10 = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_dir-AP_task-rest10_run-00{item:01d}_bold')
+    dwi = create_key('sub-{subject}/{session}/dwi/sub-{subject}_{session}_dir-{dir}_run-{item}_dwi')
+    fmap = create_key('sub-{subject}/{session}/fmap/sub-{subject}_{session}_dir-{dir}_run-{item}_epi')
+    info = {anat: [], func_video: [], func_pictures: [], func_rest5: [], func_rest10: [], dwi: [], fmap:[] }
+    
+    for idx, s in enumerate(seqinfo):
+        if 't2_tse_tra_p3_noisereduction' in s.protocol_name:
+            info[anat].append(s.series_id)
+        if (s.dim1 == 64) and (s.dim2 == 64) and ('cmrr_mbep2d_bold_videos_AP' in s.protocol_name):
+            info[func_video].append(s.series_id)
+        if (s.dim1 == 64) and (s.dim2 == 64) and ('cmrr_mbep2d_bold_Pictures_AP' in s.protocol_name):
+            info[func_pictures].append(s.series_id)
+        if (s.dim1 == 64) and (s.dim2 == 64) and ('cmrr_mbep2d_bold_Resting_10' in s.protocol_name):
+            info[func_rest10].append(s.series_id)
+        if (s.dim1 == 64) and (s.dim2 == 64) and ('cmrr_mbep2d_bold_Resting_5' in s.protocol_name):
+            info[func_rest5].append(s.series_id)
+        if (s.dim1 == 96) and (s.dim2 == 96) and ('cmrr_mbep2d_diff_AP_mb4_norm_pulse1.5x' in s.protocol_name):
+            info[dwi].append({'item': s.series_id, 'dir':'AP'})
+        if (s.dim1 == 96) and (s.dim2 == 96) and ('cmrr_mbep2d_diff_PA_mb4_norm_pulse1.5x' in s.protocol_name):
+            info[dwi].append({'item': s.series_id, 'dir':'PA'})
+        if (s.dim1 == 64) and (s.dim2 == 64) and ('cmrr_mbep2d_se_AP' in s.protocol_name):
+            info[fmap].append({'item': s.series_id, 'dir':'AP'})
+        if (s.dim1 == 64) and (s.dim2 == 64) and ('cmrr_mbep2d_se_PA' in s.protocol_name):
+            info[fmap].append({'item': s.series_id, 'dir':'PA'})
+
+    print('*** infotodict')
+    print(info)
+
+    return info
+
+# # Get a numeric subject ID for BIDS format from list of DICOM file names
